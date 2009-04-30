11a592e9aea5952d4e76ba8fd7b13ef2f5403a4e
Dan Hixon <danhixon@gmail.com>
2009-04-30 14:11:29 -0700

    from locale is the system default and to locale is the current locale

--- lib/edit_in_place.rb
+++ lib/edit_in_place.rb
@@ -3,9 +3,9 @@ module EditInPlace
   def translate(msgid)
     return msgid if msgid.start_with?("<span")
     if $translate_in_same_window
-      "<span class=\"translate\" onClick=\"javascript:top.document.location.href='/translate/edit?key=#{CGI::escape(msgid)}';\">" + I18n.translate(msgid) + '</span>' 
+      "<span class=\"translate\" onClick=\"javascript:top.document.location.href='/#{I18n.locale}/translate/edit?key=#{CGI::escape(msgid)}';\">" + I18n.translate(msgid) + '</span>' 
     else
-      "<span class=\"translate\" onClick=\"window.open('/translate/edit?key=#{CGI::escape(msgid)}','mywin','width=480,height=600,scrollbars=1,toolbar=0,resizable=1');\">" + I18n.translate(msgid) + '</span>' 
+      "<span class=\"translate\" onClick=\"window.open('/#{I18n.locale}/translate/edit?key=#{CGI::escape(msgid)}','mywin','width=480,height=600,scrollbars=1,toolbar=0,resizable=1');\">" + I18n.translate(msgid) + '</span>' 
     end
   end
   alias :t :translate
--- lib/translate/routes.rb
+++ lib/translate/routes.rb
@@ -1,7 +1,7 @@
 module Translate
   class Routes
-    def self.translation_ui(map)
-      map.with_options(:controller => 'translate') do |t|
+    def self.translation_ui(map, options={})
+      map.with_options(options.merge(:controller => 'translate')) do |t|
         t.translate_list 'translate'
         t.translate 'translate/translate', :action => 'translate'
         t.translate_reload 'translate/reload', :action => 'reload'
