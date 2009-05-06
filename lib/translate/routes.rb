eac7b86a8be1ed580321f593f42c37d975de16df
Dan Hixon <danhixon@gmail.com>
2009-05-06 13:10:11 -0700

    added path prefix for locale, probably should pass it in as a parameter though

--- lib/translate/routes.rb
+++ lib/translate/routes.rb
@@ -2,10 +2,10 @@ module Translate
   class Routes
     def self.translation_ui(map, options={})
       map.with_options(options.merge(:controller => 'translate')) do |t|
-        t.translate_list 'translate'
-        t.translate 'translate/translate', :action => 'translate'
-        t.translate_reload 'translate/reload', :action => 'reload'
-        t.translate_edit 'translate/edit', :action => 'edit'
+        t.translate_list 'translate', :path_prefix=>"/:locale"
+        t.translate 'translate/translate', :action => 'translate', :path_prefix=>"/:locale"
+        t.translate_reload 'translate/reload', :action => 'reload', :path_prefix=>"/:locale"
+        t.translate_edit 'translate/edit', :action => 'edit', :path_prefix=>"/:locale"
       end
     end
   end
